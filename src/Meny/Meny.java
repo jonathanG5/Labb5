68e6016e8967100616afc592c45299b19d5a850a
Jonathan <jonathan.ghebreselasie05@hotmail.com>
2025-12-12 14:41:20 +0100

    commit

--- src/Meny/Meny.java
+++ src/Meny/Meny.java
@@ -2,7 +2,20 @@ package Meny;
 
 import MovieDatabase.MovieDatabase;
 
+import java.util.Scanner;
+
 public class Meny {
     private MovieDatabase db;
+    private Scanner scan = new Scanner(System.in);
+
+    public Meny(MovieDatabase db) {
+        this.db = db;
+    }
+
+    public void  start()
+    {
+        System.out.println("");
+    }
+
 
 }
