f8ef31241c317785e15938df4e1101a2a79f7a81
Jonathan <jonathan.ghebreselasie05@hotmail.com>
2025-12-12 13:31:10 +0100

    commit

--- src/MovieDatabase/MovieDatabase.java
+++ src/MovieDatabase/MovieDatabase.java
@@ -7,7 +7,11 @@ import java.util.ArrayList;
 public class MovieDatabase {
     private ArrayList<Movie> movies = new ArrayList<>();
     public ArrayList<Movie> getMovies() {
+        movies.add(new Movie("Training day", 5));
+        movies.add(new Movie("White chicks", 5));
+        movies.add(new Movie("Friday", 5));
         return movies;
+
     }
 
 }
