8217108c5a22c4c2a148b134c148da738c9c3643
Jonathan <jonathan.ghebreselasie05@hotmail.com>
2025-12-12 13:20:38 +0100

    commit

--- src/MovieDatabase/MovieDatabase.java
+++ src/MovieDatabase/MovieDatabase.java
@@ -1,5 +1,13 @@
 package MovieDatabase;
 
+import Movie.Movie;
+
+import java.util.ArrayList;
+
 public class MovieDatabase {
+    private ArrayList<Movie> movies = new ArrayList<>();
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
 
 }
