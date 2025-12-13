53c60d6c646d188770a23caeadcbaabbb845d22b
Jonathan <jonathan.ghebreselasie05@hotmail.com>
2025-12-13 12:00:17 +0100

    commit

--- src/Meny/Meny.java
+++ src/Meny/Meny.java
@@ -1,7 +1,9 @@
 package Meny;
 
+import Movie.Movie;
 import MovieDatabase.MovieDatabase;
 
+import java.lang.classfile.instruction.SwitchCase;
 import java.util.Scanner;
 
 public class Meny {
@@ -14,7 +16,41 @@ public class Meny {
 
     public void  start()
     {
-        System.out.println("");
+        int function = 0;
+        while(function != 4) {
+            System.out.println("--Movie Database--");
+            System.out.println("1.Search Title");
+            System.out.println("2.Search Rating");
+            System.out.println("3.Add Movie");
+            System.out.println("4.Close Program");
+            function = scan.nextInt();
+
+            switch(function) {
+                case 1 -> ;
+                case 2 -> searchrating();
+                case 3 -> addMovie();
+                case 4 -> ;
+            }
+        }
+    }
+
+    public void searchrating()
+    {
+        System.out.println("Enter minumum rating of movie (1-5)");
+        int minumum = scan.nextInt();
+        db.score(searchrating()) = minumum;
+
+
+    }
+
+    public void addMovie()
+    {
+        System.out.println("Title: ");
+        String title = scan.nextLine();
+
+        System.out.println("Review score (1-5): ");
+        int rating = scan.nextInt();
+        db.addMovie(new Movie(title,rating));
     }
 
 
