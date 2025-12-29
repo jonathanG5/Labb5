8cb6ca52ff2254a0791ce08b9f7790cf3d40b408
Jonathan <jonathan.ghebreselasie05@hotmail.com>
2025-12-29 15:53:53 +0100

    commit

--- src/MovieDatabase/databasetxt.java
+++ src/MovieDatabase/databasetxt.java
@@ -4,11 +4,18 @@ import Movie.Movie;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Klassen databasetxt ansvarar för att spara och läsa filmer till och från en textfil.
+ * Filen används som lagring av filmerna och betygen.
+ */
 public class databasetxt {
 
+
     /**
+     * Sparar en lista med filmer till textfilen "movies.txt".
+     * Varje film sparas på en egen rad i formatet: titel;betyg.
      *
-     * @param movies
+     * @param movies lista med filmer som ska sparas till filen
      */
     public static void saveMovies(ArrayList<Movie> movies) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("movies.txt"))) {
@@ -19,16 +26,23 @@ public class databasetxt {
             }
 
         } catch (IOException e) {
-            e.printStackTrace(); // fångar felet, pogrammet kraschar inte, ändra stacktrace
+            System.out.println("Kunde inte spara filmer till fil.");
         }
     }
 
+    /**
+     * Läser in filmer från textfilen "movies.txt".
+     * De filmerna som följer kravet hämtas ut och används i de andra metoderna i programmet.
+     *
+     * @return en lista med Movie-objekt som lästs in från filen.
+     * Returnerar en tom lista om filen inte finns eller inte kan läsas.
+     */
     public static ArrayList<Movie> loadMovies() {
         ArrayList<Movie> movies = new ArrayList<>();
 
         File file = new File("movies.txt");
         if (!file.exists()) {
-            return movies; // om filen inte finns än, kommer tillbaka med tom lista
+            return movies;
         }
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -43,7 +57,7 @@ public class databasetxt {
             }
 
         } catch (IOException e) {
-            e.printStackTrace(); //fångar felet, pogrammet kraschar inte
+            System.out.println("Kunde inte läsa filmer från fil.");
         }
 
         return movies;
