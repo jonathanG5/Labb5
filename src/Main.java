bb8c85e729ea5f1fb2a55b29b4867d13e1386764
Jonathan <jonathan.ghebreselasie05@hotmail.com>
2025-12-29 15:56:37 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 import Meny.Meny;
 import MovieDatabase.MovieDatabase;
 
+/**
+ * Main-klassen är programmets startpunkt.
+ * Här används databasen och menyn, och programmet startas.
+ */
 public class Main {
+    /**
+     * Skapar databasen och läser in filmer från fil
+     * Skapar menyn och skickar med databasen
+     * Startar menyloopen
+     * @param args
+     */
     public static void main(String[] args) {
         MovieDatabase Movies = new MovieDatabase();
         Meny meny = new Meny(Movies);
